5b184280ee17c51c7c5519f1e49146439b503b65
Julian Unkel <julian.unkel@gmail.com>
2020-06-06 16:31:55 +0200

    Added continouous palette functionality

--- R/tol.R
+++ R/tol.R
@@ -1,4 +1,4 @@
-tol_palettes_qualitative <- list(
+tol_palettes <- list(
   bright = c(blue = "#4477AA",
              cyan = "#66CCEE",
              green = "#228833",
@@ -48,14 +48,27 @@ tol_palettes_qualitative <- list(
             yellow = "#EEDD88",
             orange = "#EE8866",
             pink = "#FFAABB",
-            grey = "#DDDDDD")
+            grey = "#DDDDDD"),
+  sunset = c("#364B9A", "#4A7BB7", "#6EA6CD", "#98CAE1", "#C2E4EF", "#EAECCC",
+             "#FEDA8B", "#FDB366", "#F67E4B", "#DD3D2D", "#A50026"),
+  BuRd = c("#2166AC", "#4393C3", "#92C5DE", "#D1E5F0", "#F7F7F7", "#FDDBC7",
+           "#F4A582", "#D6604D", "#B2182B"),
+  PRGn = c("#762A83", "#9970AB", "#C2A5CF", "#E7D4E8", "#F7F7F7", "#D9F0D3",
+           "#ACD39E", "#5AAE61", "#1B7837"),
+  YlOrBr = c("#FFFFE5", "#FFF7BC", "#FEE391", "#FEC44F", "#FB9A29", "#EC7014",
+             "#CC4C02", "#993404", "#662506")
 )
 
 
-get_palette_tol <- function(palette = "vibrant", reverse = FALSE, order = NULL) {
+get_palette_tol <- function(palette = "vibrant", reverse = FALSE, order = NULL,
+                            discrete = TRUE, ...) {
 
   # Extract palette
-  pal <- tol_palettes_qualitative[[palette]]
+  pal <- tol_palettes[[palette]]
+
+  if (discrete) {
+
+  }
 
   # Custom order
   if (!is.null(order)) pal <- pal[order]
@@ -63,32 +76,53 @@ get_palette_tol <- function(palette = "vibrant", reverse = FALSE, order = NULL) 
   # Reverse
   if (reverse) pal <- rev(pal)
 
-  function(n) {
+  # Discrete output
+  if (discrete) {
 
-    if (n > length(pal)) {
-      warning(glue::glue('Palette "{palette}" contains only {length(pal)} colors, while data needs {n}'))
+    function(n) {
+
+      if (n > length(pal)) {
+        warning(glue::glue('Palette "{palette}" contains only {length(pal)} colors, ',
+                           'while data needs {n}.',
+                           'Consider setting discrete = FALSE'))
+      }
+
+      unname(pal[1:n])
     }
 
-    unname(pal[1:n])
+  } else {
+    # Interpolate values
+    colorRampPalette(pal, ...)
   }
+
 
 }
 
 
 scale_colour_tol <- function(palette = "vibrant",
-                           reverse = FALSE, order = NULL, ...) {
-  ggplot2::discrete_scale("colour", palette,
-                          palette = get_palette_tol(palette, reverse, order),
-                          ...)
+                            reverse = FALSE, order = NULL, discrete = TRUE, ...) {
+  if (discrete) {
+    ggplot2::discrete_scale("colour", palette,
+                            palette = get_palette_tol(palette, reverse, order, discrete),
+                            ...)
+  } else {
+    ggplot2::scale_color_gradientn(colours = get_palette_tol(palette, reverse, order,
+                                                            discrete)(256))
+  }
 
 }
 
 scale_color_tol <- scale_colour_tol
 
 scale_fill_tol <- function(palette = "vibrant",
-                       reverse = FALSE, order = NULL, ...) {
-  ggplot2::discrete_scale("fill", palette,
-                          palette = get_palette_tol(palette, reverse, order),
-                          ...)
+                           reverse = FALSE, order = NULL, discrete = TRUE, ...) {
+  if (discrete) {
+    ggplot2::discrete_scale("fill", palette,
+                            palette = get_palette_tol(palette, reverse, order, discrete),
+                            ...)
+  } else {
+    ggplot2::scale_fill_gradientn(colours = get_palette_tol(palette, reverse, order,
+                                                             discrete)(256))
+  }
 
 }
